271bb56a7e62bce3ce7a6dc9b4574aeb428b1c38
Alex Tomas <atomasllimos@gmail.com>
2022-12-11 11:51:26 +0100

    feat: Add useToggle

---
+++ useToggle/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import ToggleComponent from './components/ToggleComponent'
+function App() {
+  return (
+    <div className="container">
+      <ToggleComponent />
+    </div>
+  )
+}
+
+export default App
