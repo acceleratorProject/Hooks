c00a9816d82056ee02200b112959e759721965a2
erunan-josu <josu.hernandez92@gmail.com>
2022-12-12 23:40:13 +0100

    feat: useEventListener

---
+++ useEventListener/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import EventListenerComponent from './components/EventListenerComponent'
+function App() {
+  return (
+    <div className='container'>
+      <EventListenerComponent/>
+    </div>
+  )
+}
+
+export default App
