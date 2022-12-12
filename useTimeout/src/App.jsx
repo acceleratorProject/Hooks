0ae9c8b6da2443c851ddaa304615c762ff9794ed
erunan-josu <josu.hernandez92@gmail.com>
2022-12-12 12:51:29 +0100

    feat: useTimeout

---
+++ useTimeout/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import TimeoutComponent from './components/TimeoutComponent'
+function App() {
+  return (
+    <div className="container">
+      <TimeoutComponent />
+    </div>
+  )
+}
+
+export default App
