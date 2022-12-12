ca5b586c4e2bb4762bb1c5cc4391f1666bd743cf
Alex Tomas <atomasllimos@gmail.com>
2022-12-12 12:03:45 +0100

    feat: useStateWithValidation

---
+++ useStateWithValidation/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import StateWithValidationComponent from './components/StateWithValidationComponent'
+function App() {
+  return (
+    <div className='container'>
+      <StateWithValidationComponent />
+    </div>
+  )
+}
+
+export default App
